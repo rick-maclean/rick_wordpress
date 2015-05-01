55383bb7010f7fb37f764cc646995e8a39159f6a
Rick Maclean <rick_maclean@sil.org>
2015-05-01 16:40:40 -0500

    more programming and moving code around in content.php file

--- wp-content/themes/childoftwentytwelve/no-title-page.php
+++ wp-content/themes/childoftwentytwelve/no-title-page.php
@@ -10,22 +10,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 				
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							
-							<div class="entry-content">
-								<?php the_content(); ?>
-								<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
-							</div><!-- .entry-content -->
-							<footer class="entry-meta">
-								<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
-							</footer><!-- .entry-meta -->
-						</article><!-- #post -->
-				
-				
-				
-				
-				
-				
+				<?php get_template_part( 'content', 'page' ); ?>
 				
 				<?php comments_template( '', true ); ?>
 			<?php endwhile; // end of the loop. ?>
